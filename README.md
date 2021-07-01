ce0f7325f9ac82150c7c59bb262c510431611fd1
jobsonmedeiros <jobsonmm@gmail.com>
2021-06-30 21:41:09 -0300

    Criado routes

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # iniciando-com-nodejs
+- Instalação e configuração do ambiente
 ## Criação de uma API simples com o framework Express
-
+- CRUD com Node.JS
 Requisitos:
 - Conhecer JS e fundamentos;
 - Conhecer comandos básicos de terminal
--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require("express") // instala o framework express
 const app = express() // express como função
 const port = 3000 // vai rodar em http://localhost:3000
 
-app.listen(port, (req, res) => {
-    console.log("Node rodando na porta " + port + "...")
-})
+// função de callback que dirá quando a API está pronta para ser
+// usada.
+app.listen(port, () => {console.log("API rodando na porta " + port + "...")})
+
+// Mensagem tentando acessar o servidor via http: "Cannot GET /"
+// Precisamos liberar o método http - a "/" vai ser o root da aplicação
+app.get("/", (req, res) => {res.send(("Olá, mundo. Estou usando o express :D"))}) 
+
+// Mensagem ok. Express adicionado como dependência com sucesso!
 
