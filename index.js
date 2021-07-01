7fbd5b45d96abdbe8b0abf8a29760b210d68386a
jobsonmedeiros <jobsonmm@gmail.com>
2021-07-01 09:05:54 -0300

    Caminho ativo - alteração de __dirname para '/'

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require("express") // instala o framework express
+const express = require('express') // instala o framework express
+// Usando a modularização do node vamos importar o routes
+const userRoute = require("./routes/userRoute.js")
+
 const app = express() // express como função
 const port = 3000 // vai rodar em http://localhost:3000
 
-// função de callback que dirá quando a API está pronta para ser
-// usada.
-app.listen(port, () => {console.log("API rodando na porta " + port + "...")})
+// vamos usar a função userRoute passando como dependência nosso app
+// userRoute(app) // vamos usar o postman pra testes
+userRoute(app)
 
 // Mensagem tentando acessar o servidor via http: "Cannot GET /"
 // Precisamos liberar o método http - a "/" vai ser o root da aplicação
@@ -12,3 +15,6 @@ app.get("/", (req, res) => {res.send(("Olá, mundo. Estou usando o express :D"))
 
 // Mensagem ok. Express adicionado como dependência com sucesso!
 
+// função de callback que dirá quando a API está pronta para ser
+// usada.
+app.listen(port, () => {console.log("API rodando na porta " + port + "...")})
---
+++ routes/userRoute.js
@@ -0,0 +1,38 @@
+// Criando uma simulação de rota de usuário (criar e ler)
+const fs = require("fs") // módulo p/ lidar com arquivos de sistema
+const {join} = require('path') // para lidar com pastas de arquivos
+
+// Vamos simular um banco de dados com um arquivo JSON
+const filePath = join('/', 'users.json') // users - lista de usuários
+
+// Vamos criar um método simples para consultar o usuário
+const getUsers = () => {
+    const data = fs.existsSync(filePath) // se o arquivo filePath existe
+    ? fs.readFileSync(filePath) // vamos abrir o arquivo de maneira síncrona
+    : [] // se o arquivo não existir retorne um objeto vazio
+
+    try { // vamos tratar qualquer erro que houver
+        return JSON.parse(data)
+    } catch (error) {
+        return [] // havendo algum problema na leitura retornaremos um objeto vazio
+    }
+}
+
+// Vamos criar um método para salvar o usuário
+
+const saveUser = (users) => fs.writeFileSync(filePath, JSON.stringify(users, null, '\t')) 
+// vamos fazer tabulação pra cada user
+
+// passamos como dependência o próprio app
+const userRoute = (app) => {
+    // a rota /users cuidará de todas as requisições get/post/put/delete
+    // id pode ser usado como parâmetro adicional 
+    app.route('/users/:id?')
+    // vamos criar o get
+    .get((req, res) => {
+        const users = getUsers() // usa a função pra ler e
+        res.send({users}) // envia resposta com os usuários
+    })
+}
+
+module.exports = userRoute // exportando o módulo para usar externamente
